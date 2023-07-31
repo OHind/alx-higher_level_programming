042574572720c5f2ad846028db421e5f28a83c6f
OHind <ouchani.hind@gmail.com>
2023-07-31 15:29:32 -0700

    Using the format to print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    if x == 0
-        print()
-        return i
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-        print()
-    except IndexError:
-        print()
-        return i
-    return i + 1
+    count = 0
+    for i in range(x):
+        try:
+            print('{}'.format(my_list[i]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        except IndexError:
+            break
+    print()
+    return count
 
