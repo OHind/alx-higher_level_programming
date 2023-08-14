26e1eb039127fcda1084e53687525a15ba817e5e
OHind <ouchani.hind@gmail.com>
2023-08-14 10:22:48 -0700

    A class with not implemented method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A improved base geometry class based on an empty class"""
+
+
+class BaseGeometry:
+    """Represent base geometry"""
+
+    def area(self):
+        """Area method not implemented"""
+        raise Exception("area() is not implemented")
