7adbd5387475bb78c34b4c9e6fc678689d9e63fd
OHind <ouchani.hind@gmail.com>
2023-08-14 15:36:13 -0700

    inherit and use the super method

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """__init__ method
+        Args:
+            width (int): width integer must be greater than 0
+            height (int): height integer must be greater than 0
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
