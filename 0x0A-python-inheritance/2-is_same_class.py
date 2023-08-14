b9edaf34210d947961ca007a5a5669166a22e567
OHind <ouchani.hind@gmail.com>
2023-08-14 09:07:15 -0700

    Some pycode styling

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,6 +2,7 @@
 """ Determin if the object is exactly the specified instance of the specified class"""
 
 
+
 def is_same_class(obj, a_class):
     """Check if an object is an instance of a given class
 
