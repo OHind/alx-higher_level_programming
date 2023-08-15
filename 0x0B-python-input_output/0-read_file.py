5c93992e247b1f78eb4d81f93be81a36a4f6ad26
OHind <ouchani.hind@gmail.com>
2023-08-15 13:38:33 -0700

    Remove the new line after the print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
         filename: the file to read
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
