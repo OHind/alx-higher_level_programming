5915ad26799a072d646dd554c3040e6d59e5c752
OHind <ouchani.hind@gmail.com>
2023-09-21 14:54:31 -0700

    Tasks : 12, 13, 14 , 15 and 16

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Upadate the score of Bob
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- Compute the score average of all records
+SELECT AVG(score) AS average_score FROM second_table;
