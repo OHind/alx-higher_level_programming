90ae60d7edcbed5320467f793e94de507af38911
OHind <ouchani.hind@gmail.com>
2023-11-04 08:31:07 -0700

    101-stored.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const oldDict = require('./101-data').dict;
-const newDict = {};
-for (const val in oldDict) {
-  if (oldDict[val] in newDict) {
-    newDict[oldDict[val]].push(val);
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
   } else {
-    newDict[oldDict[val]] = [val];
+    newDict[dict[key]].push(key);
   }
 }
 console.log(newDict);
