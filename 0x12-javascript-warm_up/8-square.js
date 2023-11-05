25c6cbcb326b2e82d9b1a5eaa4a722b70253979c
OHind <ouchani.hind@gmail.com>
2023-11-05 05:30:15 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-const x = process.argv[2];
-
-if (isNaN(x)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('X'.repeat(x));
+if (parseInt(process.argv[2])) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
+      process.stdout.write('X');
+    }
+    console.log('');
   }
+} else {
+  console.log('Missing size');
 }
