43e70631fdd158fbfe982f9a4527778905c2de09
OHind <ouchani.hind@gmail.com>
2023-11-05 04:02:24 -0800

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Lists 10 commits from the most recent to the oldest
-of the repository "rails" by the user rails
-"""
-
-import sys
+"""The Holberton School staff evaluates candidates"""
 import requests
+import sys
 
-if __name__ == "__main__":
-    repo_name = sys.argv[1]
+try:
     owner = sys.argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo_name)
-
-    request = requests.get(url)
-    commits = request.json()
-    try:
-        for i in range(10):
-            print('{}: {}'.format(
-                commits[i].get('sha'),
-                commits[i].get('commit').get('author').get('name')))
-    except IndexError:
-        pass
+    repo = sys.argv[1]
+    url = 'https://api.github.com/repos/'+owner+"/"+repo+"/commits"
+    res = requests.get(url)
+    res = res.json()
+    cont = 1
+    for el in res:
+        commit = el.get('commit')
+        sha = el.get('sha')
+        author = commit.get('author').get('name')
+        print("{}: {}".format(sha, author))
+        if cont == 10:
+            break
+        cont += 1
+except Exception as e:
+    print(e)
